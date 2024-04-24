4aba80a01f1f962d02cfa4bfcdc21dc8e428e0c4
stvo12 <158223819+stvo12@users.noreply.github.com>
2024-04-23 23:19:08 -0600

    Update main.c

--- PreLab5/PreLab5/main.c
+++ PreLab5/PreLab5/main.c
@@ -1,253 +1,96 @@
 /*
- * Functions.c
+ * PreLab5.c
  *
- * Created: 15/04/2024 11:28:25 p. m.
- *  Author: e-mel
+ * Created: 15/04/2024 09:50:21 p. m.
+ * Author : e-mel
  */ 
 
-#include "Functions.h"
+#define F_CPU 16000000
+#include "Functions/Functions.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdint.h>
+#include <util/delay.h>
 
-void initADC(uint8_t just, uint8_t canal, uint8_t prescale){
-	ADMUX = 0;
-	
-	// Vref = 5V
-	ADMUX |= (1<<REFS0);
-	ADMUX &= ~(1<<REFS1);
-	
-	ADCSRA = 0;
-	ADCSRA |= (1<<ADEN); //Enciende ADC
-	ADCSRA |= (1<<ADIE); //Int ADC
-	
-	//Justificación 1(Izq), 0(Der)
-	if(just == 1){
-		ADMUX |= (1 << ADLAR);
-	}
-	else{
-		ADMUX &= ~(1 << ADLAR);
-	}
-	
-	//Prescaler (cases)
-	switch (prescale){
-		case 2:
-			ADCSRA &= ~((1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2));
-			break;
-		case 4:
-			ADCSRA |= (1 << ADPS1);
-			ADCSRA &= ~((1 << ADPS0) | (1 << ADPS2));
-			break;
-		case 8:
-			ADCSRA |= (1 << ADPS0 ) | (1 << ADPS1);
-			ADCSRA &= ~(1 << ADPS2);
-			break;
-		case 16:
-			ADCSRA |= (1 << ADPS2);
-			ADCSRA &= ~((1 << ADPS0) | (1 << ADPS1));
-			break;
-		case 32:
-			ADCSRA |= (1 << ADPS0 ) | (1 << ADPS2);
-			ADCSRA &= ~(1 << ADPS1);
-			break;
-		case 64:
-			ADCSRA |= (1 << ADPS1) | (1 << ADPS2);
-			ADCSRA &= ~(1 << ADPS0);
-			break;
-		case 128:
-			ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
-			break;
-		default:
-			ADCSRA &= ~((1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2));
-	}
-	
-	if (canal < 6){
-		switch(canal){
-			case 0:
-				DIDR0 |= (1<<ADC0D);
-			break;
-			case 1:
-				DIDR0 |= (1<<ADC1D);
-			break;
-			case 2:
-				DIDR0 |= (1<<ADC2D);
-			break;
-			case 3:
-				DIDR0 |= (1<<ADC3D);
-			break;
-			case 4:
-				DIDR0 |= (1<<ADC4D);
-			break;
-			case 5:
-				DIDR0 |= (1<<ADC5D);
-			break;
-			default: DIDR0 &= ~(1<<ADC0D);
-		}
-	}
+uint8_t ValorADC = 0;
+uint8_t Pot = 0;
+uint8_t Pot2 = 0;
+uint8_t Pot2PWM = 0;
+uint8_t Pot3 = 0;
+uint8_t Cycle = 0;
+uint8_t PWM_Manual = 0;
+uint8_t CounterPWM_M = 0;
+
+uint16_t ADCtoServo(uint8_t ValorADC){
+	return (uint16_t)(((float)ValorADC/255)*(124.9-62.45)+62.45);
 }
 
-uint16_t readADC(uint16_t canal){
-	ADMUX = (ADMUX & 0xF0) | canal;
-	ADCSRA |= (1<<ADSC);
-	while (ADCSRA & (1 << ADSC));
-		return ADC;
-}
-
-void initPWM0(){
-	DDRD |= (1 << PD6);         // Configurar PD6 (OC0A) como salida
-
-	// Configuración del Timer0 para PWM
-	TCCR0A |= (1 << COM0A1) | (1 << WGM00) | (1 << WGM01); // Modo PWM rápido, no inversor
-	TCCR0B |= (1 << CS00);      // Sin prescaler
-}
-
-void initPWM1FastA(uint8_t inverted, uint8_t mode){
-	DDRB |= (1 << DDB1);
-	TCCR1A = 0;
-	// Invertido o no
-	if(inverted == 1){
-		TCCR1A |=  (1 << COM1A1) | (1 << COM1A0);
-	}
-	else {
-		TCCR1A |= (1 << COM1A1);
-	}
+void setup(void){
+	DDRC = 0;
 	
-	// Modo
-	switch (mode){
-		case 0:
-			TCCR1A |= (1 << WGM10);
-		break;
-		case 1:
-			TCCR1A |= (1 << WGM11);
-		break;
-		case 2:
-			TCCR1A |= (1 << WGM10) | (1 << WGM11);
-		break;
-		case 3:
-			TCCR1A |= (1 << WGM10);
-		break;
-		case 4:
-			TCCR1A |= (1 << WGM10) | (1 << WGM11);
-		break;
-		
-		default:
-			TCCR1A |= (1 << WGM10);
-	}
+}
+
+void initT0(){
+	TCCR0A |=  (1 << WGM00) | (1 << WGM01);
+	TCCR0B |= (1 << CS00); // Sin preescaler
+	// Habilitar la interrupción de comparación de Timer0
+	TIMSK0 |= (1 << OCIE0A);
 	
-	//ICR1H = 0x04;
-	//ICR1L = 0XE1;
+	//Valor Comp
+	OCR0A = PWM_Manual;
 }
 
-void initPWM1FastB(uint8_t prescaler, uint8_t mode){
-	TCCR1B = 0;
-	// Prescaler
-	switch (prescaler){
-		case 0:
-			TCCR1B |= (1 << CS10);
-		break;
-		case 1:
-			TCCR1B |= (1 << CS11);
-		break;
-		case 2:
-			TCCR1B |= (1 << CS10) | (1 << CS11);
-		break;
-		case 3:
-			TCCR1B |= (1 << CS12);
-		break;
-		case 4:
-			TCCR1B |= (1 << CS10) | (1 << CS12);
-		break;
-		default:
-			TCCR1B |= (1 << CS10);
-	}
-	// Modo
-	switch (mode){
-		case 0:
-			TCCR1B |= (1 << WGM12);
-		break;
-		case 1:
-			TCCR1B |= (1 << WGM12);
-		break;
-		case 2:
-			TCCR1B |= (1 << WGM12);
-		break;
-		case 3:
-			TCCR1B |= (1 << WGM12) | (1 << WGM13);
-		break;
-		case 4:
-			TCCR1B |= (1 << WGM12) | (1 << WGM13);
-		break;
-		default:
-			TCCR1B |= (1 << WGM12);
-	}
-}
-
-void initPWM2FastA(uint8_t inverted, uint8_t mode){
-	DDRB |= (1 << DDB3);
-	TCCR2A = 0;
-	// Invertido o no
-	if(inverted == 1){
-		TCCR2A |=  (1 << COM2A1) | (1 << COM2A0);
-	}
-	else {
-		TCCR2A |= (1 << COM2A1);
-	}
+int main(void)
+{
+	DDRB |= (1<<PD6);
+	initPWM0();
 	
-	// Modo
-	switch (mode){
-		case 0:
-		TCCR2A |= (1 << WGM20) | (1<< WGM21);
-		break;
-		case 1:
-		TCCR2A |= (1 << WGM20) | (1 << WGM21);
-		break;
-		
-		default:
-		TCCR2A |= (1 << WGM20) | (1 << WGM21);
-	}
+	initPWM1FastA(0, 0);
+	initPWM1FastB(4, 0);
 	
-	//ICR1H = 0x04;
-	//ICR1L = 0XE1;
+	initPWM2FastA(0, 0);
+	initPWM2FastB(4, 0);
+	
+	initADC(1,6,128);
+	_delay_ms(100);
+	initADC(1,7,128);
+	_delay_ms(100);
+	initADC(1,5,128);
+	
+	sei();
+    /* Replace with your application code */
+    while (1) 
+    {
+		readADC(6);
+		Pot2 = ValorADC;
+		//Pot2PWM = ADCtoServo(Pot2);
+		updateDutyCycleA(Pot2/6);
+		_delay_ms(10);
+		readADC(7);
+		Pot3 = ValorADC;
+		updateDutyCycleB(Pot3/6);
+		_delay_ms(10);
+		readADC(5);
+		PWM_Manual = ValorADC;
+		OCR0A = PWM_Manual;
+		_delay_ms(5);
+    }
 }
 
-void initPWM2FastB(uint8_t prescaler, uint8_t mode){
-	TCCR2B = 0;
-	// Prescaler
-	switch (prescaler){
-		case 0:
-		TCCR2B |= (1 << CS20);
-		break;
-		case 1:
-		TCCR2B |= (1 << CS21);
-		break;
-		case 2:
-		TCCR2B |= (1 << CS22);
-		break;
-		case 3:
-		TCCR2B |= (1 << CS22) | (1 << CS21);
-		break;
-		case 4:
-		TCCR2B |= (1 << CS20) | (1 << CS21) | (1 << CS22);
-		break;
-		default:
-		TCCR2B |= (1 << CS20);
+ISR(ADC_vect){
+	ValorADC =  ADCH;
+	ADCSRA |= (1<<ADIF);
+}
+
+ISR(TIMER0_COMPA_vect) {
+	// Incrementar el contador
+	CounterPWM_M++;
+
+	// Si el contador alcanza el valor máximo, reiniciar el contador y poner la salida en alto
+	if (CounterPWM_M >= 255) {
+		PORTB |= (1 << PD6);
+		CounterPWM_M = 0;
+		} else if (CounterPWM_M >= PWM_Manual) { // Si el contador alcanza el valor deseado, poner la salida en bajo
+		PORTB &= ~(1 << PD6);
 	}
-	// Modo
-	switch (mode){
-		case 0:
-		TCCR2B &= ~(1 << WGM22);
-		break;
-		case 1:
-		TCCR2B |= (1 << WGM22);
-		break;
-		default:
-		TCCR2B &= ~(1 << WGM22);
-	}
-}
-
-void updateDutyCycleA(uint8_t Cycle){
-	OCR1A = Cycle;
-}
-
-void updateDutyCycleB(uint8_t Cycle){
-	OCR2A = Cycle;
 }
